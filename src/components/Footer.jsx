ae521d56928bbe369380e1c5bb123d4c11e37b46
alokalok10 <125990188+alokalok10@users.noreply.github.com>
2023-10-10 21:01:40 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://ssahibsingh.github.io" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Alok Prabhat</a>
+              <a  href="https://github.com/alokalok10/alokalok" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Alok Prabhat</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+            <a className="text-dark fs-4" href="https://github.com/alokalok10/alokalok" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
